89f412bd77cba898f0d1e6f5324b6c451180f5db
Tom Gundersen <teg@jklm.no>
2016-09-04 21:37:21 +0200

    tests: test REGISTER/UNREGISTER ioctls
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- tools/testing/selftests/udrm/test-api.c
+++ tools/testing/selftests/udrm/test-api.c
@@ -9,13 +9,15 @@
 
 #define _GNU_SOURCE
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <linux/udrm.h>
 #include "test.h"
 
 /* make sure /dev/udrm exists, is a cdev and accessible */
 static void test_api_cdev(void)
 {
 	struct stat st;
-	int r;
+	int r, fd;
 
 	r = access(test_path, F_OK);
 	assert(r >= 0);
@@ -24,9 +26,22 @@ static void test_api_cdev(void)
 	assert(r >= 0);
 	assert((st.st_mode & S_IFMT) == S_IFCHR);
 
-	r = open(test_path, O_RDWR | O_CLOEXEC | O_NONBLOCK | O_NOCTTY);
+	fd = open(test_path, O_RDWR | O_CLOEXEC | O_NONBLOCK | O_NOCTTY);
+	assert(fd >= 0);
+
+	r = ioctl(fd, UDRM_CMD_REGISTER, NULL);
 	assert(r >= 0);
-	close(r);
+
+	r = ioctl(fd, UDRM_CMD_REGISTER, NULL);
+	assert(r < 0 && errno == EALREADY);
+
+	r = ioctl(fd, UDRM_CMD_UNREGISTER, NULL);
+	assert(r >= 0);
+
+	r = ioctl(fd, UDRM_CMD_UNREGISTER, NULL);
+	assert(r >= 0);
+
+	close(fd);
 
 	return TEST_OK;
 }
